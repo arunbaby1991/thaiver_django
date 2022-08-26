d09c06f84fc16f9a330fbeb55a165df350b8b543
arunbaby1991 <arun.b@inapp.com>
2022-08-26 09:41:15 +0530

    deleted pychache

--- test_project/polls/views.py
+++ test_project/polls/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import  Question
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'hello.html', {'name': 'arun'})
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('hello.html')
+    return HttpResponse(template.render({"name": output}, request))
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'hello.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'thaiver_django',
+       'USER': 'thaiver',
+       'PASSWORD': 'thaiver',
+       'HOST': 'localhost',
+       'PORT': '5432',
     }
 }
 
@@ -122,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
