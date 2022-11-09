fa855b65a5ca49ae549878464808c2f46243aa59
arunbaby1991 <arun.b@inapp.com>
2022-11-09 22:12:19 +0530

    edited the admin

--- test_project/polls/admin.py
+++ test_project/polls/admin.py
@@ -3,11 +3,21 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+# The admin interface has the ability to edit models on the same page as a parent model.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+
 # There is also a decorator for registering your ModelAdmin classes
 @admin.register(Question)
 class AuthorAdmin(admin.ModelAdmin):
     # To display the columns in the admin panel for questions
     list_display = ('question_text', 'weightage')
+
+    # Once click on + sign for choice we can add question and choice in the same page
+    inlines = [
+        ChoiceInline,
+    ]
 
 
 admin.site.register(Choice)
--- test_project/polls/views.py
+++ test_project/polls/views.py
@@ -11,13 +11,17 @@ def index(request):
     output = ', '.join([q.question_text for q in latest_question_list])
     template = loader.get_template('hello.html')
     return HttpResponse(template.render({"name": output}, request))
+
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'hello.html', {'question': question})
 
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
