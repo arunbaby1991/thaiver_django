cb8ed86f3107500eedc2fc4cdf3c1daf78a457b9
arunbaby1991 <arun.b@inapp.com>
2022-11-09 21:55:07 +0530

    edited the admin

--- test_project/polls/admin.py
+++ test_project/polls/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+# There is also a decorator for registering your ModelAdmin classes
+@admin.register(Question)
+class AuthorAdmin(admin.ModelAdmin):
+    # To display the columns in the admin panel for questions
+    list_display = ('question_text', 'weightage')
+
 
 admin.site.register(Choice)
--- test_project/polls/models.py
+++ test_project/polls/models.py
@@ -5,11 +5,13 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    weightage = models.IntegerField(null=True)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
 
+    # If the views calls this function using the Question obj they get the true or false
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
